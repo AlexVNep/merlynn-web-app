a3016bca1a5fc670f19a57f8e28f9cab3dba2d3c
Alex <alexvnep@gmail.com>
2025-03-01 10:12:07 +0200

    Logic to handle registration

--- app/ui/RegisterForm.tsx
+++ app/ui/RegisterForm.tsx
@@ -2,26 +2,26 @@
 
 import RegisterButton from "../components/RegisterButton";
 
-// import { useRouter } from "next/navigation";
-// import { RegisterWithCreds } from "@/actions/auth";
-// import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { registerWithCreds } from "../actions/actions";
+import { useState } from "react";
 
 export default function RegisterForm() {
-  // const router = useRouter();
-  // const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
 
-  // async function handleSubmit(formData: FormData) {
-  //   const result = await RegisterWithCreds(formData);
+  async function handleSubmit(formData: FormData) {
+    const result = await registerWithCreds(formData);
 
-  //   if ("error" in result) {
-  //     setError(result.error);
-  //   } else if ("success" in result) {
-  //     router.push("/sign-in"); // Redirect only if registration is successful
-  //   }
-  // }
+    if ("error" in result) {
+      setError(result.error);
+    } else if ("success" in result) {
+      router.push("/login");
+    }
+  }
   return (
     <div>
-      <form action="" className="w-full flex flex-col gap-4">
+      <form action={handleSubmit} className="w-full flex flex-col gap-4">
         <div>
           <label className="block text-sm font-medium text-gray-200">
             Email
@@ -46,7 +46,7 @@ export default function RegisterForm() {
             className="mt-1 w-full px-4 p-2  h-10 rounded-md border border-gray-200 bg-white text-sm text-gray-700"
           />
         </div>
-        {/* {error && <p className="text-red-500">{error}</p>} */}
+        {error && <p className="text-red-500">{error}</p>}
         <div className="mt-4">
           <RegisterButton />
         </div>
