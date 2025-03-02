2fce4a11032d03fb746a0c8c985e3fe0ee017ef5
Alex <alexvnep@gmail.com>
2025-03-02 12:30:14 +0200

    Feat: Add dynamic form rendered based ondata sent via searchParams

---
+++ app/ui/DynamicForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useState } from "react";
+import { Attribute, EndpointState, Value } from "../utils/definitions";
+
+export default function DynamicFormComponent({
+  state,
+}: {
+  state: EndpointState;
+}) {
+  const [formData, setFormData] = useState({});
+
+  if (!state || !state?.data?.metadata) {
+    return <p className="text-red-500">Invalid API response</p>;
+  }
+
+  const { attributes } = state?.data?.metadata;
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Submitted Data:", formData);
+  };
+
+  return (
+    <div className="w-[400px] mt-10 ">
+      <div>
+        <h2 className="text-2xl w-full text-center font-bold mb-6">
+          Model: {state.data.name}
+        </h2>
+      </div>
+      <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 p-4">
+        {/* Dynamic Attributes */}
+        {attributes.map((attr: Attribute, index: number) => (
+          <div key={index}>
+            <label className="block text-sm font-medium text-gray-200">
+              {attr.question}
+            </label>
+            {attr.domain.type === "text" ? (
+              <select
+                name={attr.question}
+                onChange={handleChange}
+                className="mt-1 w-full p-2 border border-gray-400 rounded-md bg-white text-black"
+              >
+                {attr.domain.values.map((val: Value, i: number) => (
+                  <option key={i} value={val.value}>
+                    {val.value}
+                  </option>
+                ))}
+              </select>
+            ) : attr.domain.type === "number" ? (
+              <input
+                type="number"
+                name={attr.question}
+                min={attr.domain.lowerbound}
+                max={attr.domain.upperbound}
+                onChange={handleChange}
+                className="mt-1 w-full p-2 border border-gray-400 rounded-md bg-white text-black"
+              />
+            ) : null}
+          </div>
+        ))}
+
+        <button
+          type="submit"
+          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
