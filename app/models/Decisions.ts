0ebb54cc7ec0685ee223d2b2dce54fa3b5742e54
Alex <alexvnep@gmail.com>
2025-03-03 08:26:55 +0200

    Create model for data to be saved on DB

---
+++ app/models/Decisions.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+import { DecisionData } from "../utils/definitions";
+
+const { model, models } = mongoose;
+
+const decisionSchema = new Schema<DecisionData>(
+  {
+    id: { type: String, required: true },
+    modelInput: { type: Map, of: Schema.Types.Mixed, required: true },
+    userInput: { type: Map, of: Schema.Types.Mixed, required: true },
+    decision: { type: String, required: true },
+    confidence: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+const ModelDecision =
+  models.ModelDecision || model<DecisionData>("ModelDecision", decisionSchema);
+
+export default ModelDecision;
